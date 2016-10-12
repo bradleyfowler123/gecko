da57204dab70c0d004980519c8bd6c6d2478104e
Bradley <bradley.fowler123@gmail.com>
2016-10-12 16:35:44 +0100

    Working - Homepage TextList added

--- app/src/main/java/com/auton/bradley/myfe/HomeFragment.java
+++ app/src/main/java/com/auton/bradley/myfe/HomeFragment.java
@@ -4,9 +4,13 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.ListFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 
 public class HomeFragment extends Fragment {
 
@@ -23,6 +27,14 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_home,container,false);
+
+        String[] weekForecast = getResources().getStringArray(R.array.testArray);
+
+        ListAdapter adapter = new ArrayAdapter<>(getActivity(),R.layout.home_list_item, weekForecast);
+        ListView home_list = (ListView) rootView.findViewById(R.id.home_list);
+        home_list.setAdapter(adapter);
+
+        return rootView;
     }
 }
