b3fd1f3dd20285987ef524f847d683308dfb215d
Bradley <bradley.fowler123@gmail.com>
2016-10-27 18:33:11 +0100

    Working; large; - Registering and Logging in fully working

--- app/src/main/java/com/auton/bradley/myfe/LoginActivity.java
+++ app/src/main/java/com/auton/bradley/myfe/LoginActivity.java
@@ -1,6 +1,7 @@
 package com.auton.bradley.myfe;
 
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,11 +9,16 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class LoginActivity extends AppCompatActivity {
 
     Button loginButton;
-    EditText email, password;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,20 +26,58 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
         loginButton = (Button) findViewById(R.id.login_button);
-        email = (EditText) findViewById(R.id.editText_login_email);
-        password = (EditText) findViewById(R.id.editText_login_password);
+        final EditText etEmail = (EditText) findViewById(R.id.editText_login_email);
+        final EditText etPassword = (EditText) findViewById(R.id.editText_login_password);
 
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(getBaseContext(),"Searching!",Toast.LENGTH_SHORT).show();
+              //  Toast.makeText(getBaseContext(),"Searching!",Toast.LENGTH_SHORT).show();
+
+                final String email = etEmail.getText().toString();
+                final String password = etPassword.getText().toString();
+
+                Response.Listener<String> responseListener = new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject jsonResponse = new JSONObject(response);
+                            boolean success = jsonResponse.getBoolean("success");
+                         //   Toast.makeText(getBaseContext(),"Test",Toast.LENGTH_SHORT).show();
+                            if(success) {
+
+                              //  Toast.makeText(getBaseContext(),"Your in!",Toast.LENGTH_SHORT).show();
+                                String name = jsonResponse.getString("name");
+                                String dob = jsonResponse.getString("dob");
+
+                                //Intent intent = new Intent(LoginActivity.this, LoginActivity.class);
+                                //LoginActivity.this.startActivity(intent);
+
+                                Toast.makeText(getBaseContext(),"Hi " + name + ", you were born on " + dob,Toast.LENGTH_SHORT).show();
+
+                            } else {
+                                AlertDialog.Builder builder = new AlertDialog.Builder(LoginActivity.this);
+                                builder.setMessage("Login Failed")
+                                        .setNegativeButton("Retry", null)
+                                        .create()
+                                        .show();
+                            }
+                        }catch (JSONException e){
+                            e.printStackTrace();
+                        }
+                    }
+
+                };
+
+                LoginRequest loginRequest = new LoginRequest(email,password,responseListener);
+                RequestQueue queue = Volley.newRequestQueue(LoginActivity.this);
+                queue.add(loginRequest);
             }
         });
 
     }
-
+        // takes user to register page
     public void onCreateAccClick(View view) {
-
         Intent intent = new Intent(this, RegisterActivity.class);
         startActivity(intent);
     }
