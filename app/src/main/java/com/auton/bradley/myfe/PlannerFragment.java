5775d61cd55dcd74f74864c6b0829c9c7f4bfcec
Bradley <bradley.fowler123@gmail.com>
2016-10-31 19:15:52 +0000

    Not Working; small; -  tried to add tabs to profile tab

--- app/src/main/java/com/auton/bradley/myfe/PlannerFragment.java
+++ app/src/main/java/com/auton/bradley/myfe/PlannerFragment.java
@@ -1,10 +1,18 @@
 package com.auton.bradley.myfe;
 
+import android.app.Activity;
+import android.app.ActivityManager;
+import android.app.LocalActivityManager;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTabHost;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TabHost;
 import android.widget.TextView;
 
                                 // fragment that handles the planner tab
@@ -23,20 +31,47 @@ public class PlannerFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         final View rootView = inflater.inflate(R.layout.fragment_planner, container, false);        // enables easy access to the root search xml
-
-        TextView tvName = (TextView) rootView.findViewById(R.id.textView);
-        TextView tvDob = (TextView) rootView.findViewById(R.id.textView2);
+        FragmentTabHost mTabHost;
+        TextView tvName = (TextView) rootView.findViewById(R.id.tv_profile_name);
+        ImageView profilePic = (ImageView) rootView.findViewById(R.id.img_profile_pic);
 
         MainActivity activity = (MainActivity) getActivity();
         if(activity.user.loggedIn) {
-            tvName.setText("Hey " + activity.user.name +"! You were born on " + activity.user.dob);
-            tvDob.setText(activity.user.email);
+            tvName.setText(activity.user.name);
+            profilePic.setVisibility(View.VISIBLE);
+
+            mTabHost = (FragmentTabHost) getActivity().findViewById(R.id.tabhost1);
+
+            mTabHost.setup(getActivity(), getChildFragmentManager(), R.id.TabContent);
+
+            Bundle arg1 = new Bundle();
+            arg1.putString("Arg for Frag1", "a");
+
+            Bundle arg2 = new Bundle();
+            arg2.putString("Arg for Frag2", "b");
+
+            mTabHost.addTab(mTabHost.newTabSpec("a").setIndicator("a") ,ProfileAgendaTab.class, arg1);
+            mTabHost.addTab(mTabHost.newTabSpec("b").setIndicator("b"), ProfileAgendaTab.class, arg2);
+
+/*
+
+
+
+
+            host.setup(getActivity(),getChildFragmentManager());
+
+            host.addTab(host.newTabSpec("first").setIndicator("First").setContent(new Intent(getActivity()  ,ProfileAgendaTab.class )));
+            host.addTab(host.newTabSpec("second").setIndicator("Second").setContent(new Intent(getActivity(), ProfilePhotosTab.class )));
+            host.setCurrentTab(0);
+
+
+*/
+
         }
         else {
             tvName.setText("You should Sign Up");
-            tvDob.setText("");
+            profilePic.setVisibility(View.INVISIBLE);
         }
-
 
         return rootView;
     }
