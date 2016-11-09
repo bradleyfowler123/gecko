db52954d4f2eeefd26f17e1ddef9c8063ccc6a61
Bradley <bradley.fowler123@gmail.com>
2016-11-09 18:47:48 +0000

    Working; small - facebook login data retrieved

--- app/src/main/java/com/auton/bradley/myfe/LoginActivity.java
+++ app/src/main/java/com/auton/bradley/myfe/LoginActivity.java
@@ -1,14 +1,17 @@
 package com.auton.bradley.myfe;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
@@ -18,6 +21,8 @@ import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
 import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
@@ -25,9 +30,12 @@ import com.facebook.login.widget.LoginButton;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
 /**
  * Created by Bradley on 04/11/2016.
@@ -38,6 +46,7 @@ import java.util.Map;
 
 public class LoginActivity extends AppCompatActivity {
     Button loginButton;
+    Bundle bFacebookData;
     public int currentTab;
     CallbackManager callbackManager;
     LoginButton fbLoginButton;
@@ -104,11 +113,21 @@ public class LoginActivity extends AppCompatActivity {
 
 
         fbLoginButton = (LoginButton) findViewById(R.id.login_with_facebook_button);
+        fbLoginButton.setReadPermissions("email");
 
         fbLoginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
                     @Override
                     public void onSuccess(LoginResult loginResult) {
                         // App code
+                        requestUserProfile(loginResult);
+                        try {
+                            Toast.makeText(getBaseContext(),bFacebookData.get("email").toString(),Toast.LENGTH_SHORT).show();           // inform user that system is registering them
+                        }
+                        catch (Exception c) {
+                            
+                        }
+
+
                     }
 
                     @Override
@@ -127,6 +146,68 @@ public class LoginActivity extends AppCompatActivity {
 
 
     }
+
+
+
+    public void requestUserProfile(LoginResult loginResult){
+        System.out.println("onSuccess");
+        String accessToken = loginResult.getAccessToken().getToken();
+        Log.i("accessToken", accessToken);
+
+        GraphRequest request = GraphRequest.newMeRequest(loginResult.getAccessToken(), new GraphRequest.GraphJSONObjectCallback() {
+
+            @Override
+            public void onCompleted(JSONObject object, GraphResponse response) {
+                Log.i("LoginActivity", response.toString());
+                // Get facebook data from login
+                bFacebookData = getFacebookData(object);
+            }
+        });
+        Bundle parameters = new Bundle();
+        parameters.putString("fields", "id, first_name, last_name, email,gender, birthday, location");
+        request.setParameters(parameters);
+        request.executeAsync();
+    }
+
+
+    Bundle getFacebookData(JSONObject object) {
+
+        try {
+            Bundle bundle = new Bundle();
+            String id = object.getString("id");
+
+            try {
+                URL profile_pic = new URL("https://graph.facebook.com/" + id + "/picture?width=200&height=150");
+                Log.i("profile_pic", profile_pic + "");
+                bundle.putString("profile_pic", profile_pic.toString());
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+                return null;
+            }
+
+            bundle.putString("idFacebook", id);
+            if (object.has("first_name"))
+                bundle.putString("first_name", object.getString("first_name"));
+            if (object.has("last_name"))
+                bundle.putString("last_name", object.getString("last_name"));
+            if (object.has("email"))
+                bundle.putString("email", object.getString("email"));
+            if (object.has("gender"))
+                bundle.putString("gender", object.getString("gender"));
+            if (object.has("birthday"))
+                bundle.putString("birthday", object.getString("birthday"));
+            if (object.has("location"))
+                bundle.putString("location", object.getJSONObject("location").getString("name"));
+
+            return bundle;
+        }
+        catch(JSONException e) {
+            Log.d("idk","Error parsing JSON");
+            return null;
+        }
+    }
+
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
