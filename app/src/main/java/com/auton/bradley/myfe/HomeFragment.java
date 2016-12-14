b3a029f3ad11ad1ec19371d92798490114423a45
Bradley <bradley.fowler123@gmail.com>
2016-12-14 01:24:11 +0000

    Working; small  - home list item design improvments

--- app/src/main/java/com/auton/bradley/myfe/HomeFragment.java
+++ app/src/main/java/com/auton/bradley/myfe/HomeFragment.java
@@ -14,6 +14,9 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.squareup.picasso.Picasso;
+import com.squareup.picasso.RequestCreator;
+
 import java.util.ArrayList;
 
 /*
@@ -40,10 +43,24 @@ public class HomeFragment extends Fragment {
                                    // variable declarations
         View rootView = inflater.inflate(R.layout.fragment_home,container,false);                           // enables easy access to the root search xml
         ListView home_list = (ListView) rootView.findViewById(R.id.home_list);                              // locate the list object in the home tab
-        int[] images={R.drawable.altontowers,R.drawable.climbing,R.drawable.gym,R.drawable.altontowers};    // get the image data to be shown for the recommendations
+        int[] images={R.drawable.altontowers,R.drawable.climbing,R.drawable.gym,R.drawable.altontowers,R.drawable.altontowers,R.drawable.climbing,R.drawable.gym,R.drawable.altontowers};    // get the image data to be shown for the recommendations
         final String[] titles = getResources().getStringArray(R.array.recommendationsArray);                // get the names of the recommendations to display
+        final String[] pris = getResources().getStringArray(R.array.pricesArray);
+        final String[] locs = getResources().getStringArray(R.array.locationsArray);
+
+        ArrayList<String> testTitles = new ArrayList<>();
+        ArrayList<String> testLocations = new ArrayList<>();
+        ArrayList<String> testPrices = new ArrayList<>();
+        RequestCreator[] urls = new RequestCreator[8];
+        for (int i = 0; i < 8; i++) {
+            testTitles.add(titles[i]);
+            testLocations.add(locs[i]);
+            testPrices.add(pris[i]);
+            urls[i] = Picasso.with(getContext()).load(images[i]);
+
+        }
                                    // populate the list
-        homeAdapter adapter = new homeAdapter(getActivity(),titles,images);
+        homeAdapter adapter = new homeAdapter(getActivity(),testTitles, testLocations, testPrices, urls);
         home_list.setAdapter(adapter);
                                    // handle clicks on the list items
         home_list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
@@ -60,42 +77,58 @@ public class HomeFragment extends Fragment {
 
 
 
-                        // adapter used for recommendations list view in home tab
+// adapter used for to show your activities as a list view in profile agenda sub tab
 class homeAdapter extends ArrayAdapter<String> {                                                    // Define the custom adapter class for our list view
     // declare variables of this class
-    private int[] images={};
-    private String[] titles={};
+    private ArrayList<String> activityTitles;
+    private ArrayList<String> activityLocations;
+    private ArrayList<String> activityPrices;
+    private RequestCreator[] activityPics;
     private Context c;
+
     // define a function that can be used to declare this custom adapter class
-    homeAdapter(Context context, String[] titles, int[] images) {                                   // arguments set the context, titles and images for this adapter class
-        super(context, R.layout.home_list_item,titles);
-        this.c=context;
-        this.titles=titles;
-        this.images=images;
+    homeAdapter(Context context, ArrayList<String> titles, ArrayList<String> locations, ArrayList<String> prices, RequestCreator[] pics) {     // arguments set the context, texts and images for this adapter class
+        super(context, R.layout.search_results_list_item, titles);
+        this.c = context;
+        this.activityTitles = titles;
+        this.activityLocations = locations;
+        this.activityPrices = prices;
+        this.activityPics = pics;
     }
+
     // class definition used to store different views within the list view to be populated
     private class ViewHolder {
-        TextView title;                                                                             // used to store the recommendations title text view
+        TextView activityTitle;
+        TextView activityLocation;
+        TextView activityPrice;
         ImageView img;
+        ImageView imageView;
     }
+
     // function that generates the list view
     @Override
-    public @NonNull
-    View getView(int position, View convertView, @NonNull ViewGroup parent){
-        if(convertView==null) {
+    public
+    @NonNull
+    View getView(int position, View convertView, @NonNull ViewGroup parent) {
+        if (convertView == null) {
             LayoutInflater inflater = (LayoutInflater) c.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
             final ViewGroup nullParent = null;
-            convertView=inflater.inflate(R.layout.home_list_item,nullParent);
+            convertView = inflater.inflate(R.layout.search_results_list_item, nullParent);
         }
         // find the views within the list
-        final ViewHolder holder=new ViewHolder();
-        holder.title=(TextView) convertView.findViewById(R.id.home_list_item_text);
-        holder.img=(ImageView)  convertView.findViewById(R.id.home_list_item_image);
+        final ViewHolder holder = new ViewHolder();
+        holder.activityTitle = (TextView) convertView.findViewById(R.id.sr_list_item_title);
+        holder.activityLocation = (TextView) convertView.findViewById(R.id.sr_list_item_location);
+        holder.activityPrice = (TextView) convertView.findViewById(R.id.sr_list_item_price);
+        holder.img = (ImageView) convertView.findViewById(R.id.sr_list_item_image);
+        holder.imageView = (ImageView) convertView.findViewById(R.id.sr_add_to_calander);
         // populate the title and image with data for a list item
-        holder.img.setImageResource(images[position]);
-        holder.title.setText(titles[position]);
+        holder.activityTitle.setText(activityTitles.get(position));
+        holder.activityLocation.setText(activityLocations.get(position));
+        holder.activityPrice.setText(activityPrices.get(position));
+        activityPics[position].into(holder.img);
+        Picasso.with(c).load(R.drawable.ic_calander).into(holder.imageView);
         // return the updated view
         return convertView;
     }
-
 }
